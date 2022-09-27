77d18db4d5b9dcfd91b95b55f66faaba736cf4be
Lucas <mendozalucas001@gmail.com>
2022-09-27 00:31:33 -0300

    Cambios puesto de atención

---
+++ repos/I01PuestoDeAtencion/Negocio.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace I01PuestoDeAtencion
+{
+    internal class Negocio
+    {
+        private PuestoAtencion caja;
+        private Queue<Cliente> clientes;
+        private string nombre;
+
+        public Cliente Cliente
+        {
+            get{
+                return clientes.Dequeue();
+            }
+            set{
+                if (!clientes.Contains(value))
+                {
+                    clientes.Enqueue(value);
+                }
+            }
+        }
+
+        private Negocio()
+        {
+            //this.nombre = "Sin nombre";
+            this.clientes = new Queue<Cliente>();
+            this.caja = new PuestoAtencion(Enumerados.Puesto.Caja1);
+        }
+
+        public Negocio(string nombre) :this()
+        {
+            this.nombre = nombre;
+        }
+
+        public static bool operator ==(Negocio n, Cliente c)
+        {
+            return (n.clientes.Contains(c));
+        }
+        public static bool operator !=(Negocio n, Cliente c)
+        {
+            return !(n == c);
+        }
+
+        public static bool operator +(Negocio n, Cliente c)
+        {
+            bool retorno;
+            retorno = false;
+
+            if (!(n == c))
+            {
+                n.clientes.Enqueue(c);
+                retorno = true;
+            }
+
+            return retorno;
+        }
+
+        public static bool operator ~(Negocio n)
+        {
+            bool retorno;
+            retorno = false;
+
+            if(n.clientes.Count > 0)
+            {
+                retorno = (PuestoAtencion.Atender(n.clientes.Dequeue()));
+            }
+
+            return retorno;
+        }
+
+        public int ClientesPendientes
+        {
+            get { return this.clientes.Count; }
+        }
+    }
+}
---
+++ repos/I01PuestoDeAtencion/PuestoAtencion.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace I01PuestoDeAtencion
+{
+    internal class PuestoAtencion
+    {
+        private static int numeroActual;
+        private Enumerados.Puesto puesto;
+
+        public int NumeroActual
+        {
+            get { return numeroActual+1; }
+        }
+
+        public static bool Atender(Cliente cli)
+        {
+            bool retorno;
+            retorno = false;
+            
+            if(cli is not null)
+            {
+                Thread.Sleep(7);
+                retorno = true;
+            }
+
+            return retorno;
+        } 
+
+        private PuestoAtencion()
+        {
+            numeroActual = 0;
+        }
+        public PuestoAtencion(Enumerados.Puesto puesto) :this()
+        {
+            this.puesto = puesto;
+        }
+
+    }
+
+    internal class Enumerados
+    {
+        public enum Puesto
+        {
+            Caja1,
+            Caja2
+        }
+    }
+}
